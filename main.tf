f3ec407e917bcea2343858394e256a08cfa7e8f9
CodingManoj <CodingManoj@gmail.com>
2024-04-26 20:52:56 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -3,17 +3,18 @@ module "mongodb" {
     ENV                 = var.ENV
 }
 
+module "redis" {
+    source              = "./vendor/modules/redis/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+    ENV                 = var.ENV
+}
+
 # module "mysql" {
 #     source              = "./vendor/modules/mysql/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
 #     ENV                 = var.ENV
 #     AZ                  = var.AZ
 # }
 
-# module "redis" {
-#     source              = "./vendor/modules/redis/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
-#     ENV                 = var.ENV
-#     AZ                  = var.AZ
-# }
+
 
 # module "rabbitmq" {
 #     source              = "./vendor/modules/rabbitmq/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
