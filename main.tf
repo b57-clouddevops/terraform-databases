3e4414cf42761058ea2250a86ebdefff77845470
CodingManoj <CodingManoj@gmail.com>
2024-04-28 21:03:43 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -13,10 +13,7 @@ module "mysql" {
     ENV                 = var.ENV
 }
 
-
-
-# module "rabbitmq" {
-#     source              = "./vendor/modules/rabbitmq/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
-#     ENV                 = var.ENV
-#     AZ                  = var.AZ
-# }
+module "rabbitmq" {
+    source              = "./vendor/modules/rabbitmq/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+    ENV                 = var.ENV
+}
