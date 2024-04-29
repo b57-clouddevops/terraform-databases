bfde041edbbd8abf1209aecb6e079fa090108cfa
CodingManoj <CodingManoj@gmail.com>
2024-04-28 21:51:59 -0400

    Auto Push

--- vars.tf
+++ vars.tf
@@ -13,7 +13,7 @@ variable "MYSQL_ENGINE_VERSION" {}
 variable "MYSQL_INSTANCE_TYPE" {}
 variable "MYSQL_SKIP_SNAPSHOT" {}
 variable "MYSQL_PORT" {}
-variable "MYSQL_ENGINE_FAMILY {}
+variable "MYSQL_ENGINE_FAMILY" {}
 
 # Rabbitmq Variables
 variable "RABBITMQ_INSTANCE_TYPE" {}
